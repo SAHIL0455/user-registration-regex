62ae5c423941cc7aa38cf3d63bd4fc068bc8171c
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-29 09:30:39 +0530

    ADDED: User to Enter a Valid Password at least one Number

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,10 +4,10 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
         Regex regex = new Regex();
-        regex.firstName();
-        regex.lastName();
-        regex.emailID();
-        regex.phoneNumber();
+//        regex.firstName();
+//        regex.lastName();
+//        regex.emailID();
+//        regex.phoneNumber();
         regex.password();
         System.out.println(regex.toString());
     }
